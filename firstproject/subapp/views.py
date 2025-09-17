994bc74d66a06afb51e009153f481c9e7fdc1ade
shibin <shibinshahir7@gmail.com>
2025-09-18 03:08:16 +0530

    template language filters created

--- firstproject/subapp/views.py
+++ firstproject/subapp/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def create(request):
-    s={'title':'the gold','summary':'the gold ','year':2010,'success':True}
+    s={'m':[{'title':'the gold','summary':'the gold ','year':2010,'success':True},
+            {'title':'the car','summary':'the vilage ','year':2020,'success':True},
+            {'title':'the village','summary':'the major ','year':2009,'success':True}]}
     return render(request,'create.html',s)
