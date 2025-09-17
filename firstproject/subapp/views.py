252041ac9eed2956e03a9af15ee6fa207efbe0f2
shibin <shibinshahir7@gmail.com>
2025-09-18 02:57:07 +0530

    template created

--- firstproject/subapp/views.py
+++ firstproject/subapp/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hai(request):
-    return HttpResponse("hai")
+def create(request):
+    s={'title':'the gold','summary':'the gold ','year':2010}
+    return render(request,'create.html',s)
