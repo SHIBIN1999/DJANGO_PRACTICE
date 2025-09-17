5df1d5eb2fd098d701282d97dd7eb087b81b5247
shibin <shibinshahir7@gmail.com>
2025-09-18 03:20:52 +0530

    url handling

--- firstproject/subapp/views.py
+++ firstproject/subapp/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def create(request):
-    s={'m':[{'title':'the gold','summary':'the gold ','year':2010,'success':True},
-            {'title':'the car','summary':'the vilage ','year':2020,'success':True},
-            {'title':'the village','summary':'the major ','year':2009,'success':True}]}
-    return render(request,'create.html',s)
+    return render(request,'create.html')
+
+def edit(request):
+    return render(request,'edit.html')
+
+def list(request):
+    return render(request,'list.html')
