6967a57a677b8805d35578ce9283022880963582
shibin <shibinshahir7@gmail.com>
2025-09-18 10:14:33 +0530

    database creation

--- firstproject/subapp/views.py
+++ firstproject/subapp/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from . models import Student
 # Create your views here.
 def create(request):
-    s={'m':[{'im':'1.jpeg'},
-            {'im':'1.jpeg'},
-            {'im':'1.jpeg'},]}
-    return render(request,'create.html',s)
+    print(request,"ffffffffffff")
+    if request.POST:
+        title=request.POST.get('title')
+        summary=request.POST.get('summary')
+        year=request.POST.get('year')
+        obj=Student(title=title,summary=summary,year=year)
+        obj.save()
+   
+    return render(request,'create.html')
 
 def edit(request):
-    return render(request,'edit.html')
-
+   return render(request,'edit.html')
+   
 def list(request):
     return render(request,'list.html')
